11c44ba8222134b6350a5ac6f38ef0f13a0e748e
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-03 03:55:12 +0000

    bugfix toppage syntaxerror

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :books, only: [:new, :create, :index, :show, ]
-
-
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
+  resources :books, only: [:new, :create, :index, :show, ]
+  resources :users, only: [:show, :edit, :update]
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'homes/about' => 'homes#about', as: 'about'
