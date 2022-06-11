b1beff9ecc1e54dc1b5d946125749969556a1a2a
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-06-11 04:23:26 +0000

    [fix] aboutpage

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about', as: 'about'
 end
--- db/migrate/20220526012912_addintroduction_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddintroductionToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_colummn :introduction
-  end
-end
